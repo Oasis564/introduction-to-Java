2e4c4bccb6aa6885454e265fba4426063f8c9da4
osaeid rony <osaeidfire@gmail.com>
2025-03-19 18:00:40 +0000

    Introduction to java

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String sentence = "hello, world!";
+        String space = " ";
+        String sentence2 = "it is a good day so have ";
+        int v1 = 5;
+        String coffee = "Coffees";
+        System.out.println(sentence + space + sentence2 + v1 + space + coffee);
+    }
+}
